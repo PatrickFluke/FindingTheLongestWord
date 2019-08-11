47e5fd49898a6052a998b489149d2daf08f40339
Patrick Fluke <pxfluke@gmail.com>
2019-08-11 12:26:21 -0400

    Update WhereDoIBelong.js
    This one was irritating... forgot to set a catch for the last position...

--- WhereDoIBelong.js
+++ WhereDoIBelong.js
@@ -1,1 +1,21 @@
-
+function getIndexToIns(arr, num) {
+  // Find my place in this sorted array.
+  var pos = -1;
+  arr.sort(function(a, b){return a-b});
+  console.log(arr);
+  for (let i = 0; i < arr.length; i++)
+  {
+    if (num <= arr[i])
+    {
+      pos = i;
+      i = arr.length
+    }
+  }
+  if (pos < 0)
+  {
+    pos = arr.length ;
+  }
+  console.log(pos);
+  return pos;
+}
+getIndexToIns([5, 3, 20, 3], 5);
